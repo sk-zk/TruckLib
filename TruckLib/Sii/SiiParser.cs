2cabcd9440ac0e488e4450dbfec4e1434565bfe4
sk-zk <sk-zk@users.noreply.github.com>
2020-10-11 14:31:19 +0200

    Example #367 of that new intellisense feature adding random usings because I pressed tab too early

--- TruckLib/Sii/SiiParser.cs
+++ TruckLib/Sii/SiiParser.cs
@@ -5,7 +5,6 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Numerics;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Text.RegularExpressions;
 

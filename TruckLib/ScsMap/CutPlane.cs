4412543f8bb0f8e30b552c14d5a492c8e2dd7760
sk-zk <sk-zk@users.noreply.github.com>
2020-10-19 01:03:50 +0200

    Update CutPlane to 884

--- TruckLib/ScsMap/CutPlane.cs
+++ TruckLib/ScsMap/CutPlane.cs
@@ -12,7 +12,7 @@ namespace TruckLib.ScsMap
     /// <summary>
     /// Defines a plane behind which nothing will be rendered.
     /// </summary>
-    public class CutPlane : MapItem
+    public class CutPlane : PathItem
     {
         public override ItemType ItemType => ItemType.CutPlane;
 
@@ -20,10 +20,8 @@ namespace TruckLib.ScsMap
 
         protected override ushort DefaultViewDistance => KdopItem.ViewDistanceClose;
 
-        public List<INode> Nodes { get; set; }
-
         /// <summary>
-        /// Determines if the cut plane is active in one direction only.
+        /// Sets if the cut plane is active in one direction only.
         /// </summary>
         public bool OneSideOnly
         {
@@ -56,12 +54,6 @@ namespace TruckLib.ScsMap
             if (initFields) Init();
         }
 
-        protected override void Init()
-        {
-            base.Init();
-            Nodes = new List<INode>(2);
-        }
-
         /// <summary>
         /// Adds a cut plane to the map.
         /// </summary>
@@ -69,40 +61,13 @@ namespace TruckLib.ScsMap
         /// <param name="nodePositions"></param>
         /// <param name="oneSideOnly"></param>
         /// <returns></returns>
-        public static CutPlane Add(IItemContainer map, IList<Vector3> nodePositions,
+        public static CutPlane Add(IItemContainer map, IList<Vector3> positions,
             bool oneSideOnly = false)
         {
-            var cutPlane = new CutPlane();
+            var cutPlane = Add<CutPlane>(map, positions);
+
             cutPlane.OneSideOnly = oneSideOnly;
 
-            // the nodes have a forward node except for the last one
-            // which just links to itself, so I'm creating the nodes in reverse
-            // to create the references
-            var posReverse = nodePositions.Reverse().ToArray(); 
-            var nodes = new List<INode>(nodePositions.Count());
-            for(int i = 0; i < posReverse.Count(); i++)
-            {
-                var node = map.AddNode(posReverse[i]);
-                node.ForwardItem = cutPlane;
-                if(i == 0)
-                {
-                    node.ForwardItem = node;
-                    node.IsRed = true;
-                }
-                else
-                {
-                    node.ForwardItem = nodes[i - 1];
-                }
-                nodes.Add(node);
-            }
-
-            nodes.Reverse();
-            cutPlane.Nodes = nodes.ToList();
-
-            // the item is added to the sector the first node is in.
-            // if you store it somewhere else, it will still work, but 
-            // it can't be deleted until you update it
-            map.AddItem(cutPlane);
             return cutPlane;
         }
 
@@ -114,41 +79,10 @@ namespace TruckLib.ScsMap
         {
             var node = Nodes[0].Sectors[0].Map.AddNode(position);
             node.ForwardItem = this;
-            node.ForwardItem = node;
-            Nodes.Last().ForwardItem = node;
             Nodes.Add(node);
         }
 
-        /// <summary>
-        /// Moves the item to a different location, where Nodes[0] 
-        /// will be set to the given position.
-        /// </summary>
-        /// <param name="newPos"></param>
-        public override void Move(Vector3 newPos)
-        {
-            var translation = newPos - Nodes[0].Position;
-            Translate(translation);
-        }
-
-
-        public override void Translate(Vector3 translation)
-        {
-            foreach (var node in Nodes)
-            {
-                node.Move(node.Position + translation);
-            }
-        }
-
-        internal override IEnumerable<INode> GetItemNodes() =>
-            new List<INode>(Nodes);
-
         internal override INode GetMainNode() =>
             Nodes[^1];
-
-        internal override void UpdateNodeReferences(Dictionary<ulong, INode> allNodes)
-        {
-            ResolveNodeReferences(Nodes, allNodes);
-        }
-
     }
 }

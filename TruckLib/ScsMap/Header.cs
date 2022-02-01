65b53167853631e07cc2c9ed494bad0bdc509ec5
sk-zk <sk-zk@users.noreply.github.com>
2022-02-01 14:57:42 +0100

    Update header

--- TruckLib/ScsMap/Header.cs
+++ TruckLib/ScsMap/Header.cs
@@ -12,7 +12,7 @@ namespace TruckLib.ScsMap
     /// </summary>
     public class Header : IBinarySerializable
     {
-        private const int supportedVer = 889;
+        private const int supportedVer = 891;
         /// <summary>
         /// Version number of the map format.
         /// </summary>

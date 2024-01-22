70d1851249c73419a39cddbf186ba6f390989c70
sk-zk <sk-zk@users.noreply.github.com>
2024-01-22 14:38:02 +0100

    Change Add params + Xmldoc

--- TruckLib/ScsMap/Mover.cs
+++ TruckLib/ScsMap/Mover.cs
@@ -9,28 +9,46 @@ using System.Threading.Tasks;
 namespace TruckLib.ScsMap
 {
     /// <summary>
-    /// A model with a static loop animation.
+    /// An animated model which can optionally be moved along a path.
     /// </summary>
     public class Mover : PathItem, IRecalculatable
     {
+        /// <inheritdoc/>
         public override ItemType ItemType => ItemType.Mover;
 
+        /// <inheritdoc/>
         public override ItemFile DefaultItemFile => ItemFile.Aux;
 
+        /// <inheritdoc/>
         protected override ushort DefaultViewDistance => KdopItem.ViewDistanceClose;
 
+        /// <summary>
+        /// Gets or sets the view distance of the item in meters.
+        /// </summary>
         public new ushort ViewDistance
         {
             get => base.ViewDistance;
             set => base.ViewDistance = value;
         }
 
+        /// <summary>
+        /// Unit name of the model.
+        /// </summary>
         public Token Model { get; set; }
 
+        /// <summary>
+        /// Look of the model.
+        /// </summary>
         public Token Look { get; set; }
 
+        /// <summary>
+        /// Variant of the model.
+        /// </summary>
         public Token Variant { get; set; }
 
+        /// <summary>
+        /// Movement speed of the model.
+        /// </summary>
         public float Speed { get; set; }
 
         /// <summary>
@@ -39,7 +57,7 @@ namespace TruckLib.ScsMap
         public float EndDelay { get; set; }
 
         /// <summary>
-        /// Width of the path.
+        /// Width of the area along the path in which models will be spawned.
         /// </summary>
         public float Width { get; set; }
 
@@ -48,8 +66,14 @@ namespace TruckLib.ScsMap
         /// </summary>
         public uint Count { get; set; }
 
+        /// <summary>
+        /// Cached lengths of the path segments.
+        /// </summary>
         public List<float> Lengths { get; set; } 
 
+        /// <summary>
+        /// Tags.
+        /// </summary>
         public List<Token> Tags { get; set; }
 
         /// <summary>
@@ -62,7 +86,7 @@ namespace TruckLib.ScsMap
         }
 
         /// <summary>
-        /// Gets or sets if the mover is active when street lamps are on
+        /// Gets or sets if the mover is active when street lamps are on.
         /// </summary>
         public bool ActiveDuringNight
         {
@@ -126,12 +150,18 @@ namespace TruckLib.ScsMap
             set => Kdop.Flags[6] = value;
         }
 
+        /// <summary>
+        /// Gets or sets if the mover is active during bad weather.
+        /// </summary>
         public bool ActiveDuringBadWeather
         {
             get => Kdop.Flags[11];
             set => Kdop.Flags[11] = value;
         }
 
+        /// <summary>
+        /// Gets or sets if the mover is active during nice weather.
+        /// </summary>
         public bool ActiveDuringNiceWeather
         {
             get => Kdop.Flags[10];
@@ -155,7 +185,7 @@ namespace TruckLib.ScsMap
 
         /// <summary>
         /// Gets or sets if models keep ther rotation rather than turn 180°
-        /// when <c>BounceAtEnd</c> is enabled.
+        /// when <paramref name="BounceAtEnd">BounceAtEnd</paramref> is enabled.
         /// </summary>
         public bool KeepOrientationOnBounce
         {
@@ -170,6 +200,7 @@ namespace TruckLib.ScsMap
             if (initFields) Init();
         }
 
+        /// <inheritdoc/>
         protected override void Init()
         {
             base.Init();
@@ -185,29 +216,43 @@ namespace TruckLib.ScsMap
             Count = 1;
         }
 
-        public static Mover Add(IItemContainer map, IList<Vector3> positions, Token model, Token look)
+        /// <summary>
+        /// Adds a mover to the map.
+        /// </summary>
+        /// <param name="map">The map.</param>
+        /// <param name="positions">Positions of the nodes.</param>
+        /// <param name="model">Unit name of the model.</param>
+        /// <param name="look">The look of the model.</param>
+        /// <param name="variant">The variant of the model.</param>
+        /// <returns>The newly created mover.</returns>
+        public static Mover Add(IItemContainer map, IList<Vector3> positions,
+            Token model, Token look, Token variant)
         {
             var mover = Add<Mover>(map, positions);
 
             mover.Model = model;
             mover.Look = look;
+            mover.Variant = variant;
             mover.Recalculate();
 
             return mover;
         }
 
+        /// <inheritdoc/>
         public override void Move(Vector3 newPos)
         {
             base.Move(newPos);
             Recalculate();
         }
 
+        /// <inheritdoc/>
         public override void Translate(Vector3 translation)
         {
             base.Translate(translation);
             Recalculate();
         }
 
+        /// <inheritdoc/>
         public void Recalculate()
         {
             if (Nodes.Count > 1)

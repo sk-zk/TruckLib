6ff833cc74810155fc550f0e958c2444ccfca309
sk-zk <sk-zk@users.noreply.github.com>
2024-01-26 16:11:22 +0100

    Add tests

--- TruckLibTests/TruckLib/Sii/SiiParserTest.cs
+++ TruckLibTests/TruckLib/Sii/SiiParserTest.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
+using TruckLib;
 using TruckLib.Sii;
 
 namespace TruckLibTests.TruckLib.Sii
@@ -33,6 +35,85 @@ namespace TruckLibTests.TruckLib.Sii
             Assert.Equal("v1 | center2 : 1.0", unit.Attributes["variation"][1]);
 
             Assert.Equal(0, unit.Attributes["high_tess"]);
+        }
+
+        [Fact]
+        public void ParseFloats()
+        {
+            var unit = @"
+                foo : .bar {
+                    decimal_float: 1.0
+                    hex_float: &3f800000
+                    exponent: 1.312e3
+                }
+            ";
+            var parser = new SiiParser();
+            var file = parser.DeserializeFromString(unit);
+            Assert.Equal(1.0f, file.Units[0].Attributes["decimal_float"]);
+            Assert.Equal(1.0f, file.Units[0].Attributes["hex_float"]);
+            Assert.Equal(1312f, file.Units[0].Attributes["exponent"]);
+        }
+
+        [Fact]
+        public void ParseTuples()
+        {
+            var unit = @"
+                foo : .bar {
+                    float3: (1.0, 2.0, 3.0)
+                    int3: (1, 2, 3)
+                }
+            ";
+            var parser = new SiiParser();
+            var file = parser.DeserializeFromString(unit);
+            Assert.Equal(new[] { 1.0f, 2.0f, 3.0f }, file.Units[0].Attributes["float3"]);
+            Assert.Equal(new[] { 1, 2, 3 }, file.Units[0].Attributes["int3"]);
+        }
+
+        [Fact]
+        public void ParseToken()
+        {
+            var unit = @"
+                foo : .bar {
+                    valid_token: default 
+                }
+            ";
+            var parser = new SiiParser();
+            var file = parser.DeserializeFromString(unit);
+            Assert.IsType<Token>(file.Units[0].Attributes["valid_token"]);
+            Assert.Equal((Token)"default", file.Units[0].Attributes["valid_token"]);
+        }
+
+        [Fact]
+        public void ParseBooleans()
+        {
+            var unit = @"
+                foo : .bar {
+                    a: true
+                    b: false
+                }
+            ";
+            var parser = new SiiParser();
+            var file = parser.DeserializeFromString(unit);
+            Assert.True(file.Units[0].Attributes["a"]);
+            Assert.False(file.Units[0].Attributes["b"]);
+        }
+
+        [Fact]
+        public void Includes()
+        {
+            var unit = @"
+@include ""global_include.sui""
+foo : .bar {
+    some_val: 1
+@include ""unit_include.sui""
+}
+            ";
+            var parser = new SiiParser();
+            var file = parser.DeserializeFromString(unit);
+            Assert.True(file.Includes.Count == 1);
+            Assert.Equal("global_include.sui", file.Includes[0]);
+            Assert.True(file.Units[0].Includes.Count == 1);
+            Assert.Equal("unit_include.sui", file.Units[0].Includes[0]);
         }
     }
 }

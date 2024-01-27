483cf6c0bb6f69153ab054accd496e114d070ca6
sk-zk <sk-zk@users.noreply.github.com>
2024-01-27 16:19:32 +0100

    Reorder `Add` paramas + Xmldoc

--- TruckLib/ScsMap/AnimatedModel.cs
+++ TruckLib/ScsMap/AnimatedModel.cs
@@ -11,21 +11,27 @@ using System.Collections.Specialized;
 namespace TruckLib.ScsMap
 {
     /// <summary>
-    /// <para>TODO: What even is this?</para>
+    /// TODO: What even is this?
     /// </summary>
     /// <remarks>Removed from the editor item menu, but it's still in the game and the
     /// item properties dialog still works.</remarks>
     [Obsolete]
     public class AnimatedModel : SingleNodeItem
     {
+        /// <inheritdoc/>
         public override ItemType ItemType => ItemType.AnimatedModel;
 
+        /// <inheritdoc/>
         public override ItemFile DefaultItemFile => ItemFile.Aux;
 
+        /// <inheritdoc/>
         protected override ushort DefaultViewDistance => KdopItem.ViewDistanceClose;
 
         public List<Token> Tags { get; set; }
 
+        /// <summary>
+        /// Unit name of the model.
+        /// </summary>
         public Token Model { get; set; }
      
         public bool InitHidden
@@ -41,13 +47,22 @@ namespace TruckLib.ScsMap
             if (initFields) Init();
         }
 
+        /// <inheritdoc/>
         protected override void Init()
         {
             base.Init();
             Tags = new List<Token>();
         }
 
-        public static AnimatedModel Add(IItemContainer map, Token model, Vector3 position, 
+        /// <summary>
+        /// Adds an AnimatedModel to the map.
+        /// </summary>
+        /// <param name="map">The map.</param>
+        /// <param name="position">The position of the item.</param>
+        /// <param name="model">The unit name of the model.</param>
+        /// <param name="tags">A list of tags,</param>
+        /// <returns>The newly created AnimatedModel.</returns>
+        public static AnimatedModel Add(IItemContainer map, Vector3 position, Token model,
             List<Token> tags = null)
         {
             var anim = Add<AnimatedModel>(map, position);

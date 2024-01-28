0ca032951defec1d8ccb4ef562c2377b13d6f4ee
sk-zk <sk-zk@users.noreply.github.com>
2024-01-28 15:25:57 +0100

    Add `Move(Vector3, int)` overload to parh items as well

--- TruckLib/ScsMap/PathItem.cs
+++ TruckLib/ScsMap/PathItem.cs
@@ -26,10 +26,30 @@ namespace TruckLib.ScsMap
             Nodes = new List<INode>(2);
         }
 
-        /// <inheritdoc/>
+        /// <summary>
+        /// Moves the item, where the given position will be the new position
+        /// of the node at index 0, and all other nodes will be moved relative to it.
+        /// </summary>
+        /// <param name="newPos">The new position of the node.</param>
         public override void Move(Vector3 newPos)
         {
             var translation = newPos - Nodes[0].Position;
+            Translate(translation);
+        }
+
+        /// <summary>
+        /// Moves the item, where the given position will be the new position
+        /// of the node at index <paramref name="n">n/</paramref>, and all other nodes
+        /// will be moved relative to it.
+        /// </summary>
+        /// <param name="newPos">The new position of the node.</param>
+        /// <param name="n">The index of the node which will be moved to this position.</param>
+        public void Move(Vector3 newPos, int n)
+        {
+            if (n < 0 || n >= Nodes.Count)
+                throw new ArgumentOutOfRangeException(nameof(n));
+
+            var translation = newPos - Nodes[n].Position;
             Translate(translation);
         }
 

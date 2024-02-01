d18d0c6af1cad0bf3f96767a7d85648dbdfd9a0d
sk-zk <sk-zk@users.noreply.github.com>
2024-02-01 19:40:13 +0100

    Add tests

--- TruckLibTests/TruckLib/ScsMap/MapTest.cs
+++ TruckLibTests/TruckLib/ScsMap/MapTest.cs
@@ -18,5 +18,52 @@ namespace TruckLibTests.TruckLib.ScsMap
             Assert.Throws<ArgumentNullException>(() => map.Name = " ");
             Assert.Throws<ArgumentException>(() => map.Name = "a/b");
         }
+
+        [Fact]
+        public void AddSector()
+        {
+            var map = new Map("foo");
+            var sector = map.AddSector(-5, 5);
+
+            Assert.Single(map.Sectors);
+            Assert.Equal(sector, map.Sectors[(-5, 5)]);
+            Assert.Equal(-5, sector.X);
+            Assert.Equal(5, sector.Z);
+        }
+
+        [Fact]
+        public void AddNode()
+        {
+            var map = new Map("foo");
+            var node = map.AddNode(new(-69, 0, -420), true);
+
+            Assert.Single(map.Nodes);
+            Assert.Equal(node, map.Nodes[node.Uid]);
+            Assert.Equal(new(-69, 0, -420), node.Position);
+            Assert.True(node.IsRed);
+
+            Assert.Single(map.Sectors);
+            Assert.True(map.Sectors.ContainsKey((-1, -1)));
+        }
+
+        [Fact]
+        public void DeleteNode()
+        {
+            var map = new Map("foo");
+            var node = map.AddNode(new(-69, 0, -420), true);
+
+            map.Delete(node);
+
+            Assert.Empty(map.Nodes);
+        }
+
+        [Fact]
+        public void GetSectorOfCoordinate()
+        {
+            var expected = (-1, -1);
+            var actual = Map.GetSectorOfCoordinate(new(-100, 200, -300));
+
+            Assert.Equal(expected, actual);
+        }
     }
 }

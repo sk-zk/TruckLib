036eaa636ca9be98ea6873b8a60090429c4c7f2c
sk-zk <sk-zk@users.noreply.github.com>
2024-02-24 18:28:54 +0100

    Fix visibility

--- TruckLib/Sii/LinkPointer.cs
+++ TruckLib/Sii/LinkPointer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TruckLib.Sii
 {
-    internal record struct LinkPointer(string Value)
+    public record struct LinkPointer(string Value)
     {
         public static implicit operator LinkPointer(string value) => new(value);
         public static implicit operator string(LinkPointer lp) => lp.Value;

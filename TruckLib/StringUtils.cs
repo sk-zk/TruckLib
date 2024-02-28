b258f1f8f881c68db2b3c6409a43ce4b40e13fad
sk-zk <sk-zk@users.noreply.github.com>
2024-02-28 01:10:50 +0100

    Remove unused methods

--- TruckLib/StringUtils.cs
+++ TruckLib/StringUtils.cs
@@ -12,51 +12,6 @@ namespace TruckLib
     internal static class StringUtils
     {
         /// <summary>
-        /// Checks if a string contains a number in a format used by .sii files.
-        /// </summary>
-        /// <param name="str"></param>
-        /// <returns></returns>
-        public static bool IsNumerical(string str)
-        {
-            if (IsHexNotationFloat(str))
-                return true;
-
-            return double.TryParse(str, NumberStyles.Float | NumberStyles.AllowExponent, 
-                CultureInfo.InvariantCulture, out var _);
-        }
-
-        /// <summary>
-        /// Checks if a string contains a float written as hex bytes as used in .sii files,
-        /// e.g. "&amp;3f800000".
-        /// </summary>
-        public static bool IsHexNotationFloat(string str)
-        {
-            const string hexPrefix = "&";
-            return str.StartsWith(hexPrefix)
-                && str.Length == (hexPrefix.Length + 8)
-                && IsHexadecimal(str[1..]);
-        }
-
-        /// <summary>
-        /// Checks if a string contains hexadecimal digits only.
-        /// </summary>
-        /// <param name="str"></param>
-        /// <returns></returns>
-        public static bool IsHexadecimal(string str)
-        {
-            foreach (var c in str)
-            {
-                var isHex = (c >= '0' && c <= '9') ||
-                    (c >= 'a' && c <= 'f') ||
-                    (c >= 'A' && c <= 'F');
-
-                if (!isHex)
-                    return false;
-            }
-            return true;
-        }
-
-        /// <summary>
         /// Converts a byte array containing null-terminated strings
         /// to a List&lt;string&gt;.
         /// </summary>
--- TruckLibTests/TruckLib/StringUtilsTest.cs
+++ TruckLibTests/TruckLib/StringUtilsTest.cs
@@ -9,34 +9,6 @@ namespace TruckLibTests.TruckLib
 {
     public class StringUtilsTest
     {
-        [Theory]
-        [InlineData("101", true)]
-        [InlineData("-0.2", true)]
-        [InlineData("2.34446041e-15", true)]
-        [InlineData("&3a80bedf", true)]
-        [InlineData("0x42", false)]
-        public void IsNumerical(string input, bool expected)
-        {
-            Assert.Equal(expected, StringUtils.IsNumerical(input));
-        }
-
-        [Theory]
-        [InlineData("&3a80bedf", true)]
-        [InlineData("3a80", false)]
-        public void IsHexNotationFloat(string input, bool expected)
-        {
-            Assert.Equal(expected, StringUtils.IsHexNotationFloat(input));
-        }
-
-        [Theory]
-        [InlineData("DEADBEEF", true)]
-        [InlineData("0a1b2C", true)]
-        [InlineData("G", false)]
-        public void IsHexadecimal(string input, bool expected)
-        {
-            Assert.Equal(expected, StringUtils.IsHexadecimal(input));
-        }
-
         [Fact]
         public void CStringBytesToList()
         {

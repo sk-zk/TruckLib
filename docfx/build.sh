86857c80ece6437b1d70b24cfb53aaa119780e03
skzk <sk-zk@users.noreply.github.com>
2024-10-18 16:43:45 +0200

    Update build.sh

--- docfx/build.sh
+++ docfx/build.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
+scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
+cd "$scriptDir"
+
 rm -rf ./_site
 git clone https://github.com/sk-zk/TruckLib.Core.git
 git clone https://github.com/sk-zk/TruckLib.Models.git
 git clone https://github.com/sk-zk/TruckLib.Sii.git
 git clone https://github.com/sk-zk/TruckLib.HashFs.git
-docfx
+docfx ./docfx.json

4d7a3ea4afd09a469590d07977d04ff32304b75f
sk-zk <sk-zk@users.noreply.github.com>
2025-02-05 01:39:30 +0100

    Style

--- TruckLib/ScsMap/TerrainQuad.cs
+++ TruckLib/ScsMap/TerrainQuad.cs
@@ -17,16 +17,16 @@ namespace TruckLib.ScsMap
         private byte byte3;
         private byte byte4;
 
-        const int n1Mask = 0x0F;
-        const int n2Mask = 0xF0;
-        const byte noDetVegMask = 16;
+        const int N1Mask = 0x0F;
+        const int N2Mask = 0xF0;
+        const byte NoDetVegMask = 16;
 
         /// <summary>
         /// Index of the main terrain material of this quad.
         /// </summary>
         public Nibble MainMaterial
         {
-            get => (Nibble)(byte1 & n1Mask);
+            get => (Nibble)(byte1 & N1Mask);
             set => byte1 |= (byte)value;
         }
 
@@ -36,7 +36,7 @@ namespace TruckLib.ScsMap
         /// </summary>
         public Nibble BlendMaterial
         {
-            get => (Nibble)((byte1 & n2Mask) >> 4);
+            get => (Nibble)((byte1 & N2Mask) >> 4);
             set => byte1 |= (byte)((byte)value << 4);
         }
 
@@ -45,7 +45,7 @@ namespace TruckLib.ScsMap
         /// </summary>
         public Nibble Opacity
         {
-            get => (Nibble)(byte2 & n1Mask);
+            get => (Nibble)(byte2 & N1Mask);
             set => byte2 |= (byte)value;
         }
 
@@ -54,7 +54,7 @@ namespace TruckLib.ScsMap
         /// </summary>
         public Nibble ColorBottomLeft
         {
-            get => (Nibble)((byte2 & n2Mask) >> 4);
+            get => (Nibble)((byte2 & N2Mask) >> 4);
             set => byte2 |= (byte)((byte)value << 4);
         }
 
@@ -63,7 +63,7 @@ namespace TruckLib.ScsMap
         /// </summary>
         public Nibble ColorBottomRight
         {
-            get => (Nibble)(byte3 & n1Mask);
+            get => (Nibble)(byte3 & N1Mask);
             set => byte3 |= (byte)value;
         }
 
@@ -72,7 +72,7 @@ namespace TruckLib.ScsMap
         /// </summary>
         public Nibble ColorTopLeft
         {
-            get => (Nibble)((byte3 & n2Mask) >> 4);
+            get => (Nibble)((byte3 & N2Mask) >> 4);
             set => byte3 |= (byte)((byte)value << 4);
         }
 
@@ -81,7 +81,7 @@ namespace TruckLib.ScsMap
         /// </summary>
         public Nibble ColorTopRight
         {
-            get => (Nibble)(byte4 & n1Mask);
+            get => (Nibble)(byte4 & N1Mask);
             set => byte4 |= (byte)value;
         }
 
@@ -90,7 +90,7 @@ namespace TruckLib.ScsMap
         /// </summary>
         public QuadVegetation Vegetation
         {
-            get => (QuadVegetation)((byte4 & n2Mask) >> 5);
+            get => (QuadVegetation)((byte4 & N2Mask) >> 5);
             set => byte4 |= (byte)((byte)value << 5);
         }
 
@@ -99,13 +99,13 @@ namespace TruckLib.ScsMap
         /// </summary>
         public bool NoDetailVegetation
         {
-            get => (byte4 & noDetVegMask) == noDetVegMask;
+            get => (byte4 & NoDetVegMask) == NoDetVegMask;
             set
             {
                 if (value)
-                    byte4 |= noDetVegMask;
+                    byte4 |= NoDetVegMask;
                 else
-                    byte4 &= noDetVegMask ^ 0xFF;
+                    byte4 &= NoDetVegMask ^ 0xFF;
             }
         }
 
